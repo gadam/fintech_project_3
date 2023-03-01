6799fd07e6450ee67b63b4ef1cd1e82297a49592
Guy Adam <guy.adam@gmail.com>
2023-03-01 11:58:13 +1100

    Rearchitected app to use sidebar to capture purchase form

--- scripts/sales.py
+++ scripts/sales.py
@@ -1,4 +1,7 @@
-# Display the events available for sale
+#---------------------------------#
+# Display the events available    # 
+#   for sale and transact a sale  #
+#---------------------------------#
 
 import os
 import json
@@ -9,22 +12,28 @@ import pandas as pd
 import streamlit as st
 import psycopg2
 
-# Setup
-
+#---------------------------------#
+# Setup                           #
+#---------------------------------#
 load_dotenv()
 st.set_page_config(layout="wide")
+
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
 # Set the contract address (this is the address of the deployed contract)
-# owner_address = os.getenv("SMART_CONTRACT_OWNER_ADDRESS")
 # Get a list of all available accounts from Ganache
 #  and use the first account as contract owner address
 accounts = w3.eth.accounts
 owner_address = accounts[0]
-@st.cache(allow_output_mutation=True)
 
+# @st.cache(allow_output_mutation=True)
+# @st.cache_resource
 
+#---------------------------------#
+# Load contract in preparation for#
+#   sale                          #
+#---------------------------------#
 def load_contract():
     '''Load the `Tickets` smartcontract 
        to register tickets'''
@@ -40,6 +49,9 @@ def load_contract():
 
     return contract
 
+#---------------------------------#
+# Retrieve events list from db    #
+#---------------------------------#
 def load_events():
     '''Retrieve the events collected in the events table 
        and display in streamlit web page'''
@@ -64,7 +76,7 @@ def load_events():
             "event_date", 
             "venue", 
             "tkt_price_aud", 
-            "total_no_tkts", 
+            "total_no_of_tkts", 
             "tkts_remaining"
         ]
     )
@@ -74,52 +86,68 @@ def load_events():
     conn.close()
     return df
 
+#---------------------------------#
+# Display list of events for sale #
+#---------------------------------#
 def display_events(events_df):
     '''Display available events in a table and allow user to buy'''
-    cols = st.columns((2, 1, 1, 1, 1))
-    col_headings = ["Event", "Date", "Venue", "Remaining Tickets", "Action"]
-    for col, heading in zip(cols, col_headings):
-        col.write(f"##### {heading}")
-    st.markdown("---")
-    for index, row in events_df.iterrows():
-        col1, col2, col3, col4, col5 = st.columns((2, 1, 1, 1, 1))
-        col1.write(row["event_name"])
-        col2.write(row["event_date"])
-        col3.write(row["venue"])
-        col4.write(row["tkts_remaining"])
-        button_phold = col5.empty() # Create a placeholder
-        buy = button_phold.button("Buy", key=row["event_id"])
-        if buy:
-            # button_phold.empty()
-            return row["event_id"]
 
-def buy(contract, eventId, buyerName, buyerAddress):
-    '''Mint a new block for a ticket purchase'''
-    tx_hash = contract.functions.registerTicket(
-        eventId,
-        buyerName,
-        buyerAddress
-    ).transact({"from": owner_address, "gas": 100000})
-    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    return dict(receipt)
+    st.markdown("## Available events")
+    display_events_df = pd.DataFrame()
+    display_events_df["Event"] = events_df["event_name"]
+    display_events_df["Date"] = events_df["event_date"]
+    display_events_df["Venue"] = events_df["venue"]
+    display_events_df["Price"] = events_df["tkt_price_aud"]
+    display_events_df["Tickets_Available"] = events_df["tkts_remaining"]
+    st.dataframe(display_events_df, use_container_width=True)
 
-# Entry point
+
+#---------------------------------#
+# Transact the sale               #
+#---------------------------------#
+def buy(contract, events_df):
+    '''Capture buyer name and address and
+       Mint a new block for a ticket purchase'''
+    with st.sidebar:
+            
+        receipt = []
+        with st.form("buyer_details"):
+            st.markdown("## Buy tickets for:")
+            st.selectbox(
+                "### Tickets for", 
+                options=events_df[["event_name", "tkt_price_aud"]],
+                key="event_selector"
+            )
+            quantity = st.number_input("Quantity:", min_value=1)
+            buyer_name = st.text_input("Name:")
+            buyer_address = st.selectbox("Wallet account address:", accounts[1:])
+            if  st.form_submit_button("Purchase ticket", type="primary"):
+                # tx_hash = contract.functions.registerTicket(
+                #     event_row["event_id"],
+                #     buyer_name,
+                #     buyer_address
+                # ).transact({"from": owner_address, "gas": 100000})
+                # receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+                st.success(f"{buyer_name}, of {buyer_address} purchased {quantity}")
+
+
+#---------------------------------#
+# Record sale in db               #
+#---------------------------------#
+def update_sales(token):
+    '''Record the ticket sold on the sales transactions table'''
+    pass
+
+#---------------------------------#
+# Main entry point                #
+#---------------------------------#
 st.title("NFTix - Anti-scalping event ticketing system")
-st.markdown("## Available events")
 
 events_df = load_events()
-event_id = display_events(events_df)
+
+display_events(events_df)
+
 tickets_contract = load_contract()
 
-# if event_id:
-#     buyerName = "Mel Trump"
-#     buyerAddress = "0x8fee7A777cCA7E86Cdfb4531186a4A71Fa3f58B8"
-#     tokenId = buy(tickets_contract, event_id, buyerName, buyerAddress)
-#     st.write(f"Returned token: {tokenId}")
-
-#     buyerName = "Eric Trump"
-#     buyerAddress = "0x0D110FcfE2cF010D0086756C953eD355b117197B"
-#     tokenId = buy(tickets_contract, 5, buyerName, buyerAddress)
-#     st.write(f"Returned token: {tokenId}")
-# else:
-#     st.write("No tokens returned")
+# User has chosen to buy tickets for an event
+buy(tickets_contract, events_df)
